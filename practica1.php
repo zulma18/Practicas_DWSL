aa6829d272fe9cf7f02fca549edd8479ef0ddc4c
zulma18 <zuzuguevara25@gmail.com>
2024-08-20 22:26:28 -0600

    Subiendo practica 1

---
+++ practica1.php
@@ -0,0 +1,21 @@
+<?php
+//cadena
+$nombre = "Zulma";
+$apellido = "Guevara";
+
+//entero
+$edad = 25;
+
+//float
+$estatura = 1.80;
+
+//booleano
+$vivo = true;
+
+echo $nombre;
+echo '<br><br>';
+echo $edad;
+echo '<br><br>';
+echo $nombre, ', ', $apellido, ', ', $edad;
+
+?>
